37d99f7a0cce89d64b79cd76e07aa8433b943dbd
Siddardhmustplay <siddumustplaygames@gmail.com>
2025-08-01 19:18:38 +0530

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -5,7 +5,7 @@ import { GeistMono } from "geist/font/mono"
 import "./globals.css"
 
 export const metadata: Metadata = {
-  title: "v0 App",
+  title: "$RECK",
   description: "Created with v0",
   generator: "v0.dev",
 }
